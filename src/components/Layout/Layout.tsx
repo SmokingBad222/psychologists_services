4751888771dda2a0573577ed517019558e5ef7e1
Pashka Antonov <pavelcreed222@gmail.com>
2026-05-18 21:57:50 +0300

    Repair errors and build

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
+import type { StoredAuthData } from "../../types/auth";
 import Header from "../Header/Header";
 import css from "./Layout.module.css";
 
-export default function Layout() {
+interface LayoutProps {
+    authUser: StoredAuthData | null;
+    setAuthUser: React.Dispatch<React.SetStateAction<StoredAuthData | null>>;
+}
+
+export default function Layout({authUser, setAuthUser}: LayoutProps) {
     return (
         <div className={css.layout}>
-            <Header />
+            <Header authUser={authUser} setAuthUser={setAuthUser} />
             <main className={css.main}>
                 <Outlet />
             </main>
